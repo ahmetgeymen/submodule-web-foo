2dbc72dd5790b6eac67ddf0ec7db0bcfb30be70f
Ahmet Geymen <me@ahmetgeymen.com>
2021-04-19 01:35:14 +0300

    first commit

---
+++ src/main/kotlin/com/ahmetgeymen/web/foo/controllers/HelloController.kt
@@ -0,0 +1,12 @@
+package com.ahmetgeymen.web.foo.controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/hello")
+class HelloController {
+    @GetMapping
+    fun getHello() = "Hello World!"
+}
