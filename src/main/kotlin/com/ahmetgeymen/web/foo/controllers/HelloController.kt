11d67339c8907ea4799a817c2a485ec38327b485
Ahmet Geymen <ahmet@valensas.com>
2021-04-19 02:04:48 +0300

    add spring boot actuator

--- src/main/kotlin/com/ahmetgeymen/web/foo/controllers/HelloController.kt
+++ src/main/kotlin/com/ahmetgeymen/web/foo/controllers/HelloController.kt
@@ -2,11 +2,14 @@ package com.ahmetgeymen.web.foo.controllers
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/hello")
 class HelloController {
     @GetMapping
-    fun getHello() = "Hello World!"
+    fun getHello(
+        @RequestParam(value = "name", defaultValue = "World") name: String
+    ) = "Hello $name!"
 }
